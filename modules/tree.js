4d6fa05585d8930949e527c2ed39155771e027f8
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-11-02 09:45:06 +0100

    add sortArray method, make buildTree cleaner

--- modules/tree.js
+++ modules/tree.js
@@ -2,18 +2,24 @@ import Node from './node.js';
 
 export default class Tree {
   constructor(arr = null) {
-    this.root = this.buildTree(arr);
+    this.root = this.buildTree(this.sortArray(arr));
+  }
+
+  sortArray(arr) {
+    const sorted = [...new Set(arr)].sort((a, b) => a - b);
+    return sorted;
   }
 
   buildTree(arr) {
-    if (arr.length - 1 < 0) return null;
+    if (arr.length === 0) return null;
 
     const mid = Math.floor((arr.length - 1) / 2);
 
-    const root = new Node(arr[mid]);
-
-    root.setLeft(buildTree(arr.slice(0, mid)));
-    root.setRight(buildTree(arr.slice(mid)));
+    const root = new Node(
+      arr[mid],
+      this.buildTree(arr.slice(0, mid)),
+      this.buildTree(arr.slice(mid + 1)),
+    );
 
     return root;
   }
