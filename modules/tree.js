57a7e8bcc079e4c33ca32d06b05a537d4b4f2313
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-11-02 09:32:58 +0100

    move function buildTree to Tree class as method

--- modules/tree.js
+++ modules/tree.js
@@ -1,8 +1,20 @@
-import buildTree from './buildTree';
+import Node from './node.js';
 
 export default class Tree {
   constructor(arr = null) {
-    this.arr = arr;
-    this.root = buildTree(this.arr);
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr) {
+    if (arr.length - 1 < 0) return null;
+
+    const mid = Math.floor((arr.length - 1) / 2);
+
+    const root = new Node(arr[mid]);
+
+    root.setLeft(buildTree(arr.slice(0, mid)));
+    root.setRight(buildTree(arr.slice(mid)));
+
+    return root;
   }
 }
